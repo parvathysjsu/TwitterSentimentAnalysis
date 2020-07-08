199afed134b95cc45e794e9a9df754660386d412
parvathysjsu <parvathy.kannankumarathmadomkrishnan@sjsu.edu>
2020-07-08 12:32:55 -0700

    files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TwitterSentimentAnalysis
 Sentiment Analysis on Tweets for a hashtag in NodeJS
+
+
+# Create Twitter App
+1. create developer account using https://developer.twitter.com/en/apps
+2. Create app
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var Sentiment = require('sentiment');
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN
+});
 
 router.get('/tweets/:hashtagname', function (req, res, next) {
   res.send("hello");
+
+  client.get('search/tweets', { q: '#ios #swift' }, function (error, tweets, response) {
+    tweets.statuses.forEach(function (tweet) {
+      console.log("tweet: " + tweet.text)
+    });
+  });
+
 });
+/*
+router.get('/image', function (req, res, next) {  
+
+  const textToImage = require('text-to-image');
+textToImage.generate('Lorem ipsum dolor sit amet').then(function (dataUri) {
+  console.log(dataUri);
+  res.send(dataUri);
+});
+
+textToImage.generate('Lorem ipsum dolor sit amet', {
+  debug: true,
+  maxWidth: 720,
+  fontSize: 18,
+  fontFamily: 'Arial',
+  lineHeight: 30,
+  margin: 5,
+  bgColor: "blue",
+  textColor: "red"
+}).then(function (dataUri) {
+  console.log(dataUri);
+});
+
+});*/
+
 module.exports = router;
