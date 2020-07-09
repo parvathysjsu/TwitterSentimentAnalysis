774adaf905f088a5f4305bac655abf013affa6b8
parvathysjsu <parvathy.kannankumarathmadomkrishnan@sjsu.edu>
2020-07-08 17:33:22 -0700

    changes graph result

--- routes/route.js
+++ routes/route.js
@@ -2,23 +2,76 @@ var express = require('express');
 var router = express.Router();
 var Sentiment = require('sentiment');
 var Twitter = require('twitter');
-
+var properties = require('./../config/properties')
 var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  bearer_token: process.env.TWITTER_BEARER_TOKEN
+  consumer_key: properties.twitterKeys.consumer_key,
+  consumer_secret: properties.twitterKeys.consumer_secret,
+  access_token_key: properties.twitterKeys.access_token_key,
+  access_token_secret: properties.twitterKeys.access_token_secret
 });
+router.get('/', function (req, res, next) {
+  res.render('index')
+})
+router.post('/analysis', function (req, res, next) {
+  console.log(req.body.query);
+  let hashtagName = req.body.query;
+  let positiveTweets = 0;
+  let negativeTweets = 0;
+  let neutralTweets = 0;
+  let result = {};
+  let tweetRecords = [];
+  var sentiment = new Sentiment();
+  client.get('search/tweets', { q: hashtagName, count: 100 }, function (error, tweets, response) {
+    tweets.statuses.forEach(function (tweet) {
+      // console.log("-----"+JSON.stringify(tweet))
+      // console.log("-----" + i + "-----tweet: " + tweet.text)     
+      var res_score = sentiment.analyze(tweet.text);
+      if (res_score["score"] > 0)
+        positiveTweets = positiveTweets + 1;
+      else if (res_score["score"] < 0)
+        negativeTweets = negativeTweets + 1;
+      else
+        neutralTweets = neutralTweets + 1;
+        tweetRecords.push({
+          "tweet": tweet.text.replace(/[\r\n]+/gm," "),
+          "score":res_score["score"],
+          "positive":res_score["positive"],
+          "negative":res_score["negative"]
+        })    
+    });
+    result.query=req.body.query;
+    result.positiveTweets=positiveTweets;
+    result.negativeTweets=negativeTweets;
+    result.neutralTweets=neutralTweets;
+    result.tweets=tweetRecords;
+    res.render('result', {
+      res: result
+    })
+  });  
+})
 
 router.get('/tweets/:hashtagname', function (req, res, next) {
-  res.send("hello");
-
-  client.get('search/tweets', { q: '#ios #swift' }, function (error, tweets, response) {
+  //  res.send("hello")
+  let hashtagName = '#' + req.params.hashtagname//+"&count=100";
+  let i = 1;
+  let positiveTweets = 0;
+  let negativeTweets = 0;
+  let neutralTweets = 0;
+  var sentiment = new Sentiment();
+  client.get('search/tweets', { q: hashtagName, count: 15 }, function (error, tweets, response) {
     tweets.statuses.forEach(function (tweet) {
-      console.log("tweet: " + tweet.text)
+      // console.log("-----"+JSON.stringify(tweet))
+      console.log("-----" + i + "-----tweet: " + tweet.text)
+      i = i + 1;
+      var res_score = sentiment.analyze(tweet.text);
+      console.log('%c%s', 'color: #00a3cc', res_score["score"]);
+      console.log('%c%s', 'color: #00a3cc', res_score["positive"]);
+      console.log('%c%s', 'color: #00a3cc', res_score["negative"]);
     });
   });
-
+  res.send("hello")
 });
+
 /*
 router.get('/image', function (req, res, next) {  
 
